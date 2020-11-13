3897b129a6a48a4e8b4d8a3d3844615ec869107f
Vladas <vladas.zvonkus@gmail.com>
2020-11-13 09:23:43 +0200

    Analysis image

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "vcoin.h"
 #include <omp.h>
 #include <chrono>
+#include <algorithm>
 
 #define USERS_DATA_PATH "users.dat"
 #define TRANSACTIONS_DATA_PATH "transactions.dat"
@@ -14,7 +15,7 @@ int main(int argc, char** argv) {
 
     IO::genRandUsers(users, 1000, 100, 1000000);
     IO::writeUsersToFile(USERS_DATA_PATH, users);
-    IO::genRandTransactions(transactions, users, 10000, 1, 10000, 3600*7);
+    IO::genRandTransactions(transactions, users, 1000, 1, 10000, 3600*7);
     IO::writeTransactionsToFile(TRANSACTIONS_DATA_PATH, transactions);
 
     VBlock genesisBlock;
@@ -26,12 +27,13 @@ int main(int argc, char** argv) {
     validateTransactions(transactions);
 
     const std::string miners[5] = { "1A", "1B", "1C", "1D", "1E" };
+    long long minTime = LLONG_MAX, maxTime = 0;
     double totalMineTime = 0;
     while (!transactions.empty()) {
         int winnerIndex = 0;
         auto start = std::chrono::steady_clock::now();
         auto end = std::chrono::steady_clock::now();
-#pragma omp parallel default(none) shared(chain, users, transactions, miners, winnerIndex, end, std::cout) num_threads(5)
+#pragma omp parallel default(none) shared(chain, users, transactions, miners, winnerIndex, start, end, minTime, maxTime, std::cout) num_threads(5)
         {
             VUsers pUsers(users);
             VTransactions pTransactions(transactions);
@@ -43,6 +45,8 @@ int main(int argc, char** argv) {
             Miner::mine(block, &chain, omp_get_thread_num() * 1000);
             if (chain.insert(block)) {
                 end = std::chrono::steady_clock::now();
+                maxTime = std::max(maxTime, std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count());
+                minTime = std::min(minTime, std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count());
                 updateUsersBalance(users, block.transactions);
                 IO::writeUsersToFile(USERS_DATA_PATH, pUsers);
                 IO::writeTransactionsToFile(TRANSACTIONS_DATA_PATH, pTransactions);
@@ -51,6 +55,7 @@ int main(int argc, char** argv) {
         }
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
         totalMineTime += elapsed.count();
+
         std::cout << miners[winnerIndex] << " has finished mining in " << 1.0*elapsed.count()/1000 << "s!\n";
         std::cout << "========MINED BLOCK========\n";
         chain.get(chain.head()).printHeader();
@@ -63,8 +68,9 @@ int main(int argc, char** argv) {
         std::cout << "Remaining transactions: " << transactions.size() << "\n\n";
     }
 
-    std::cout << "Average block mine time: " << totalMineTime/chain.size()/1000 << "s\n\n";
-
+    std::cout << "Average block mine time: " << totalMineTime/chain.size()/1000 << "s\n";
+    std::cout << "Minimum block mine time: " << 1.0*minTime/1000 << "s\n";
+    std::cout << "Maximum block mine time: " << 1.0*maxTime/1000 << "s\n\n";
     std::cout << "\nFinal blockchain:\n";
     int currentBlockIndex = chain.size();
     std::string currentBlock = chain.head();
